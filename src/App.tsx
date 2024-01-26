01747c32d45a97ca50846a26ad233f03df372d10
eyal-hl <eyalhermonilahat@gmail.com>
2024-01-26 22:51:33 +0200

    Added background toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import React from 'react'
 import styles from './App.module.css'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import NavBar from './components/NavBar/NavBar'
 import SignUp from './components/SignUp/SignUp'
+import classNames from 'classnames'
 const App = () => {
+    const [backgroungState, setBackgroundState] = React.useState(false)
     return (
-        <div className={styles.app}>
+        <div
+            className={classNames(styles.app, {
+                [styles.background]: backgroungState,
+            })}
+        >
             <BrowserRouter>
-                <NavBar />
+                <NavBar
+                    toggleBackground={() => {
+                        setBackgroundState((prev) => !prev)
+                    }}
+                    toggleImg={
+                        backgroungState
+                            ? '../assets/happyCricket.png'
+                            : '../assets/sadCricket.png'
+                    }
+                />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="login" element={<Login />} />
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom'
 import styles from './NavBar.module.css'
-
-const NavBar = () => {
+export interface NavBarProps {
+    toggleBackground: () => void
+    toggleImg: string
+}
+const NavBar = (props: NavBarProps) => {
     return (
         <div className={styles.navBar}>
             <Link className={styles.home} to="/">
                 <img
                     className={styles.homeImg}
-                    src="../../../assets/cricketNoBackGround.png"
+                    src="../../../assets/homeButton.png"
                 />
             </Link>
             <Link className={styles.login} to="/login">
                 Login
             </Link>
+            <div className={styles.toggle} onClick={props.toggleBackground}>
+                <img className={styles.toggleImg} src={props.toggleImg} />
+            </div>
         </div>
     )
 }
