938e61f14f7783cfe52371984e234677e4fa6db8
eyal-hl <eyalhermonilahat@gmail.com>
2024-01-27 22:23:45 +0200

    Add async keyword to getPosts function and include id property in data.json

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { PostProps } from '../components/Post/Post';
 import data from './data.json';
 
-export const getPosts = (): PostProps[] => {
+export const getPosts = async (): Promise<PostProps[]> => {
     return data.posts;
 };
