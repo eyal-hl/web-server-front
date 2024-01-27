9b491127606db4187dd127a658f6120aa953b159
eyal-hl <eyalhermonilahat@gmail.com>
2024-01-27 22:23:53 +0200

    Add Post component styles and update Post component logic

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -5,12 +5,35 @@ export interface PostProps {
     user_name: string;
     imagePath: string;
     isLiked: boolean;
+    id: number;
 }
 
 const Post = (props: PostProps) => {
-    // Implement your component logic here
-
-    return <div>{props.text}</div>;
+    return (
+        <div className={styles.post}>
+            <div className={styles.postHeader}>
+                <div className={styles.postHeaderImg}>
+                    <img src={props.imagePath} />
+                </div>
+                <div className={styles.postHeaderText}>{props.user_name}</div>
+            </div>
+            <div className={styles.postBody}>{props.text}</div>
+            <div className={styles.postFooter}>
+                <div className={styles.postFooterLike}>
+                    <img
+                        src={
+                            props.isLiked
+                                ? '../../../assets/heart.png'
+                                : '../../../assets/heartOutline.png'
+                        }
+                    />
+                </div>
+                <div className={styles.postFooterComment}>
+                    <img src="../../../assets/comment.png" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Post;
